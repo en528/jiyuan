014cf4cd0eae8f92634920e0d23846ad6d77930f
en528 <patrick9932666@gmail.com>
2026-06-16 09:55:43 +0800

    0.11

--- server.js
+++ server.js
@@ -1,42 +1,43 @@
 var express = require('express');
- 
 var NEDB = require("nedb-promises");
-var Game = new NEDB({filename:"game.db", autoload:true})
-
+var Game = new NEDB({filename:"game.db", autoload:true});
 var server = express();
+var bodyParser = require('body-parser');
 
-var bodyParser  = require('body-parser');
-server.use(bodyParser.urlencoded());
+server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 
-server.use(express.static(__dirname + '/public'));
+// ==========================================
+//Godot 4 網頁跨域標頭
+// ==========================================
+server.use((req, res, next) => {
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+});
 
-//Game.insert({score:2020, user:"MD"});
+// 設定 public 資料夾為網頁根目錄
+server.use(express.static(__dirname + '/public'));
 
 server.get('/', function(req, res){
     res.send('Hello world!');
 });
 
+// 抓取前 10 名分數
 server.get('/hiscore', function(req, res){
-     Game.find({}).sort({score:-1}).limit(10).then(result=>{
-        console.log(result);
+    Game.find({}).sort({score:-1}).limit(10).then(result=>{
         res.send(result);
-    })
-    
+    });
 });
+
+// 接收玩家上傳的分數
 server.post('/hiscore', function(req, res){
-    Game.insert(req.body);
+    var data = req.body;
+    Game.insert(data);
     res.send("OK");
 });
- 
- 
-// server.get('*', function(req, res){
-//     res.send("Page not found!", 404);
-// })
 
-//server.listen(80);
-//console.log("Web server on port 8080");
-
+//讓 Render 動態分配 Port
 var port = process.env.PORT || 8080;
 server.listen(port, () => {
     console.log("Web server is running on port " + port);
